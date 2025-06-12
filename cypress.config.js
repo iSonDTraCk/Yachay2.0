94be239fe3d9013840e35325af996d1ec1939a88
Gabriel Landa <72153409@continental.edu.pe>
2025-06-11 23:34:49 -0500

    Cypress Funcionando

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://127.0.0.1:8000', // Aquí defines tu host local
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/lesson.cy.js
+++ cypress/e2e/lesson.cy.js
@@ -1,6 +1,21 @@
-describe('Página de alumno', () => {
+describe('Página Home Principal', () => {
   it('Muestra el panel de lecciones', () => {
-    cy.visit('http://127.0.0.1:8000');
-    cy.contains('Nivel: Principiante');
+    cy.visit('http://127.0.0.1:8000'); // Asegúrate de que la URL sea correcta
+    cy.contains('DNI');
   });
+});
+
+describe('Home de alumno', () => {
+  it('Muestra el panel de lecciones', () => {
+    cy.visit('/alumno/home'); // Usa la ruta relativa, aprovecha el baseUrl
+    cy.contains('Nivel: Principiante'); // Cambia el texto por uno que realmente aparezca en tu vista
+  });
+});
+
+describe('Home de profesor', () => {
+  it('Muestra el panel de lecciones', () => {
+    cy.visit('/profesor/home'); // Usa la ruta relativa, aprovecha el baseUrl
+    cy.contains('Mis Clases'); // Cambia el texto por uno que realmente aparezca en tu vista
+  });
+  
 });
